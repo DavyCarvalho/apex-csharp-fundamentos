b49f1c4d77208311e006d59bef97341e93bfdbb3
Davy de Lima Carvalho <davycarvalho2017@hotmail.com>
2023-02-11 19:19:42 -0300

    ultimas alterações

--- Capitulo15a17ConectandoAoSqlServer/Program.cs
+++ Capitulo15a17ConectandoAoSqlServer/Program.cs
@@ -62,7 +62,7 @@ namespace CapituloQuinzeConectandoAoSqlServer
 
                 comando.ExecuteNonQuery();
 
-                Console.WriteLine("");
+                Console.WriteLine("Inserção executada com sucesso!");
             }
             catch (Exception ex)
             {
@@ -72,6 +72,116 @@ namespace CapituloQuinzeConectandoAoSqlServer
             {
                 connection.Close();
             }
+            //Inserção de dados - Fim
+
+            //Atualização de dados - Início
+
+            try
+            {
+                connection.Open();
+
+                var idDoRegistroQueSeraAtualizado = 2;
+                var novoValorDaColunaDescricao = "Carro";
+                var novoValorDaColunaQuantidade = 800;
+                var comandoSqlDeAtualizacaoDeProduto = @"UPDATE Produto
+                                                        SET Descricao = @novoValorDaColunaDescricao,
+                                                            Quantidade = @novoValorDaColunaQuantidade
+                                                        WHERE Id = @idDoRegistroQueSeraAtualizado";
+
+                var comando = new SqlCommand(comandoSqlDeAtualizacaoDeProduto, connection);
+
+                comando.Parameters.AddWithValue("@novoValorDaColunaDescricao", novoValorDaColunaDescricao);
+                comando.Parameters.AddWithValue("@novoValorDaColunaQuantidade", novoValorDaColunaQuantidade);
+                comando.Parameters.AddWithValue("@idDoRegistroQueSeraAtualizado", idDoRegistroQueSeraAtualizado);
+
+                comando.ExecuteNonQuery();
+
+                Console.WriteLine("Ação de Atualização executada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            //Atualização de dados - Fim
+
+            //Exclusão de dados - Início
+
+            try
+            {
+                connection.Open();
+
+                var idDoRegistroQueSeraApagado = 3;
+                var comandoSqlDeExclusao = "DELETE FROM Produto WHERE Id = @idDoRegistroQueSeraApagado";
+
+                var comando = new SqlCommand(comandoSqlDeExclusao, connection);
+
+                comando.Parameters.AddWithValue("@idDoRegistroQueSeraApagado", idDoRegistroQueSeraApagado);
+
+                comando.ExecuteNonQuery();
+                Console.WriteLine("Ação de Exclusão executada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            //Exclusão de dados - Fim
+
+            //Leitura de dados EXTRA - Início
+            try
+            {
+                connection.Open();
+                Console.WriteLine("A conexão foi aberta com sucesso");
+
+                var comandoSqlDeLeitura = "SELECT * FROM Produto ORDER BY Id DESC";
+
+                var comando = new SqlCommand(comandoSqlDeLeitura, connection);
+
+                var linhasRetornadasDaQuery = comando.ExecuteReader();
+
+                while (linhasRetornadasDaQuery.Read())
+                {
+                    var idDoRegistro = Convert.ToInt32(linhasRetornadasDaQuery["Id"]);
+                    var descricaoDoRegistro = linhasRetornadasDaQuery["Descricao"].ToString();
+                    var quantidadeDoRegistro = Convert.ToInt32(linhasRetornadasDaQuery["Quantidade"]);
+
+                    var objetoDeProduto = new Produto(idDoRegistro, descricaoDoRegistro, quantidadeDoRegistro);
+
+                    Console.WriteLine($"O Id do registro é: {objetoDeProduto.Id}");
+                    Console.WriteLine($"A descrição do registro é: {objetoDeProduto.Descricao}");
+                    Console.WriteLine($"A quantidade do registro é: {objetoDeProduto.Quantidade}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            //Leitura de dados EXTRA - Fim
+        }
+    }
+
+    class Produto
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(int id, string descricao, int quantidade)
+        {
+            Id = id;
+            Descricao = descricao;
+            Quantidade = quantidade;
         }
     }
 }
